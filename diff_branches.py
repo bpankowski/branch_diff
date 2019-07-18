2668fcb9a18038ed617313cd182ea0f112a5e0a6
bpankowski <bartosz.pankowski@codilime.com>
2019-07-18 14:50:54 +0200

    Added latest version after code refactor.

--- diff_branches.py
+++ diff_branches.py
@@ -1,57 +1,65 @@
 import requests
 import json
 import ast
-import unicodecsv as csv
+import csv
 import argparse
 
-# def get_data():
-#     api = 'https://api.github.com/repos/{}/commits?sha={}&access_token={}&per_page=100&page={}'.format(repos[z], branch, github_token, str(x))
-# return api
+
+def get_api(repo, branch, date, token, count):
+    api = 'https://api.github.com/repos/{}/commits?sha={}&since={}&access_token={}&per_page=100&page={}'.format(repo, branch, date, token, count)
+    return api
+
+
+def save_output(commits):
+    with open('output.csv', 'w') as output:
+         writer = csv.writer(output)
+         for value, key in commits.items():
+              writer.writerow([key, value])
+
+
+def get_data(commits_data, branch, token, date):
+      repo_response = {}
+      page_number = 0
+      repo_number = 0
+      repos = []
+      with open("repos_list") as file:
+          for line in file:
+              line = line.strip()
+              repos.append(line)
+      while repo_number != len(repos):
+          api = get_api(repos[repo_number], branch, date, token, page_number)
+          print (api)
+          page_number += 1
+          print("repository: {} page: {}".format(repos[repo_number], page_number))
+          r = requests.get(api)
+          if(r.ok):
+              repo_response = json.loads(r.text or r.content)
+          if not repo_response:
+              print("end of list.")
+              repo_number += 1
+              page_number = 0
+          for y in repo_response:
+              if y['commit']['message'] in commits_data:
+                   commits_data[y['commit']['message']] += 1
+              else:
+                   commits_data[y['commit']['message']] = 1
+          return commits_data
+
 
 def main():
-    contr = {}
+    commits_data = {}
     github_token = open("token", "r")
     github_token = github_token.read()
     parser = argparse.ArgumentParser()
     parser.add_argument("branch_master")
     parser.add_argument("branch_diff")
+    parser.add_argument("date")
     args = parser.parse_args()
     branch_master = args.branch_master
     branch_diff = args.branch_diff
-
-    repo_response = {}
-    print (branch_diff)
-    print (branch_master)
-    x = 0
-    z = 0
-    repos = []
-    with open("repos_list") as file:
-        for line in file: 
-            line = line.strip()
-            repos.append(line)
-    while z != len(repos):
-         api()
-         api = 'https://api.github.com/repos/{}/commits?sha=R5.0&access_token={}&per_page=100&page={}'.format(repos[z], github_token, str(x))
-         x += 1
-         print(api)
-         print "repository: {} page: {}".format(repos[z], x)
-         r = requests.get(api)
-         if(r.ok):
-              repo_response = json.loads(r.text or r.content)
-         if not repo_response:
-              print "end of list."
-              z += 1
-              x = 0
-         for y in repo_response:
-              # print repo_response[y]
-              if y['commit']['message'] in contr:
-                   contr[y['commit']['message']] += 1
-              else:
-                   contr[y['commit']['message']] = 1
-         with open('output.csv', 'wb') as output:
-              writer = csv.writer(output, encoding='utf-8')
-              for key, value in contr.iteritems():
-                   writer.writerow([key, value])
-
+    date = args.date
+    get_data(commits_data, branch_diff, github_token, date)
+    get_data(commits_data, branch_master, github_token, date)
+    save_output(commits_data)
 if __name__ == '__main__':
     main()
